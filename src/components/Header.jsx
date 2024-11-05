f3727e17f8fb9438189b25d910363e8834eeeef2
Mordecai <chingolo265@gmail.com>
2024-11-05 19:28:58 +0200

    context setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='flex text-2xl font-bold text-blue-700 justify-center w-full'>
+          Blood Donations Management System
+        </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/providers/DonationsProvider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DonationsProvider = () => {
+  return (
+    <div>DonationsProvider</div>
+  )
+}
+
+export default DonationsProvider
